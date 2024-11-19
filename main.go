1c40a79eb505174eefb9258d9e48da0f7cd7063b
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-19 20:36:26 +0100

    test

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ func main() {
 
 	fmt.Println("Started...")
 	timestamp := time.Now().UnixMilli()
-	logger.Info(nil, "Some logging text with {timestamp} and {username}", timestamp, "lois")
-	fmt.Println("after info")
+	logger.Info(nil, "Some logging info text at time {timestamp} by user {username}", timestamp, "lois")
 	logger.Debug(nil, "This is a debug message sent from {script}", "main")
 	logger.Warning(nil, "This is a warning!")
 	logger.Error(nil, "This is an error!")

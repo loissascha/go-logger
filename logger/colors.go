c0871cbda3d640161784154f652688270514fc83
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-19 20:07:44 +0100

    rename

--- logger/colors.go
+++ logger/colors.go
@@ -1,10 +1,10 @@
 package logger
 
 const (
-	INFO_COLOR    = "\033[34m"
-	DEBUG_COLOR   = "\033[32m"
-	WARNING_COLOR = "\033[33m"
-	ERROR_COLOR   = "\033[31m"
-	FATAL_COLOR   = "\033[35m"
-	RESET         = "\033[0m"
+	color_info    = "\033[34m"
+	color_debug   = "\033[32m"
+	color_warning = "\033[33m"
+	color_error   = "\033[31m"
+	color_fatal   = "\033[35m"
+	color_reset         = "\033[0m"
 )

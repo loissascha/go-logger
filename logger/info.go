d4b3a7223273a16a0d92fb8e3964a28a31b54e30
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-19 19:52:58 +0100

    colors

--- logger/info.go
+++ logger/info.go
@@ -12,6 +12,10 @@ type VarInfo struct {
 
 const (
 	INFO_COLOR = "\033[34m"
+	DEBUG_COLOR = "\033[32m"
+	WARNING_COLOR = "\033[33m"
+	ERROR_COLOR = "\033[31m"
+	FATAL_COLOR = "\033[35m"
 	RESET = "\033[0m"
 )
 

a13101073930be8b8d809951d308aa9fb2101b29
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-19 19:59:40 +0100

    debug

--- logger/info.go
+++ logger/info.go
@@ -17,3 +17,17 @@ func Info(err error, text string, vars ...any) {
 
 	fmt.Println(INFO_COLOR+"INFO:", text+RESET)
 }
+
+func Debug(err error, text string, vars ...any) {
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := readTextVars(text)
+	if len(res) != len(vars) {
+		fmt.Println(FATAL_COLOR+"ATTENTION: Vars in string differ from provided vars"+RESET)
+	}
+
+	text = createTextOutput(text, res, vars)
+
+	fmt.Println(DEBUG_COLOR+"DEBUG:", text+RESET)
+}

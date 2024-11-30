3e20d901f9da43bc5206128b4e4d432c5e3230e6
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-11-30 15:10:54 +0100

    new logger and setting

--- logger/logger.go
+++ logger/logger.go
@@ -1,15 +1,24 @@
 package logger
 
 type Logger struct {
-	name             string
-	color            string
-	logToFile        bool
-	showInConsole    bool
-	fileLoggingAsync bool
+	name          string
+	color         string
+	logToFile     bool
+	showInConsole bool
 }
 
-func NewLogger(name string, color string, logToFile bool) {
+func NewLogger(name string, color string, logToFile bool) *Logger {
+	r := Logger{
+		name:          name,
+		color:         color,
+		logToFile:     logToFile,
+		showInConsole: true,
+	}
+	return &r
+}
 
+func (l *Logger) ShowInConsole(show bool) {
+	l.showInConsole = show
 }
 
 func (l *Logger) Log(t string) {
